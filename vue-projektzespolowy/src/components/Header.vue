838ca1e8a83d953b8f35a49df1a513d27c265e44
Marta Kulig <bialka104b@interia.pl>
2022-03-29 21:08:55 +0200

    rozdzielono scss na mniejsze pliki, rozdzielono App.vue na komponenty, dodano multiselect

---
+++ vue-projektzespolowy/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="headerContainer">
+        <header class="header">
+            <div class="row bg-white headerRow">
+                <div class="col-6 logoContainer fw-bold">
+                  <img src="../assets/images/logo.png" alt="" class="logoImage">
+                </div>
+                <nav class="col-6 d-flex justify-content-between">
+                    <div class="navLink">
+                        <a href="" class="d-block fw-bold">Zakładka1</a>
+                    </div>
+                    <div class="navLink">
+                        <a href="" class="d-block fw-bold">Zakładka2</a>
+                    </div>
+                    <div class="navLink">
+                        <a href="" class="d-block fw-bold">Zakładka3</a>
+                    </div>
+                </nav>
+            </div>
+            <div class="container titlePage">
+                <h3>Wylicz kalorie i zadbaj o swoje zdrowie</h3>
+                <h1>FIT CALCULATOR</h1>
+                <p>Węglowodany, kalorie, białka, IG</p>
+                <button class="btn btn-primary calc mt-4">Przelicz</button>
+            </div>
+        </header>
+    </div>
+</template>
