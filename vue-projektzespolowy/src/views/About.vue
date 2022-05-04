3fbf6397f9211100687e2c11e9b0cc1de252a012
Marta Kulig <bialka104b@interia.pl>
2022-05-04 21:31:29 +0200

    stylowanie, prace wykończeniowe

--- vue-projektzespolowy/src/views/About.vue
+++ vue-projektzespolowy/src/views/About.vue
@@ -2,32 +2,48 @@
     <div class="">
         <h1 class="text-center">{{$t("header.projectCreators")}}</h1>
         <div class="row ">
-            <div class="col-12 col-md-6 my-2 aboutRow">
-                <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHXcwNIAj38Ow/profile-displayphoto-shrink_400_400/0/1625325793517?e=1657152000&v=beta&t=xR83f-YN0c0S5Gs_PiI_6FNwzIAZjwtndAbKOvameTI" class="foto"/>
-                <div class="d-inline-block px-4">
-                    <h5>Frontend Developer i Lider zespołu</h5>
-                    <p>Marta Jamróz Kulig</p>
+            <div class="col-12 col-md-6 my-2 aboutRow px-0">
+                <div class="row m-0 h-100">
+                    <div class="col-12 col-md-6 heightImg ps-md-0">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHXcwNIAj38Ow/profile-displayphoto-shrink_400_400/0/1625325793517?e=1657152000&v=beta&t=xR83f-YN0c0S5Gs_PiI_6FNwzIAZjwtndAbKOvameTI" class="foto h-100"/>
+                    </div>
+                    <div class="col-12 col-md-6 px-0 py-lg-5">
+                        <h5>Frontend Developer i Lider zespołu</h5>
+                        <p>Marta Jamróz Kulig</p>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 my-2 aboutRow">
-                <img src="./../assets/images/Michał_Drozd.jpg" class="foto"/>
-                <div class="d-inline-block px-4">
-                    <h5>Backend Developer</h5>
-                    <p>Michał Drozd</p>
+            <div class="col-12 col-md-6 my-2 aboutRow px-0">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 heightImg ps-md-0">
+                        <img src="./../assets/images/Michał_Drozd.jpg" class="foto h-100"/>
+                    </div>
+                    <div class="col-12 col-md-6 px-0 py-md-5">
+                        <h5>Backend Developer</h5>
+                        <p>Michał Drozd</p>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 my-4 aboutRow">
-                <img src="./../assets/images/Dawid_Gac.jpg" class="foto"/>
-                <div class="d-inline-block px-4">
-                    <h5>Business Analyst</h5>
-                    <p>Dawid Gac</p>
+            <div class="col-12 col-md-6 my-2 aboutRow px-0">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 heightImg ps-md-0">
+                        <img src="./../assets/images/Dawid_Gac.jpg" class="foto h-100"/>
+                    </div>
+                    <div class="col-12 col-md-6 px-0 py-md-5">
+                        <h5>Business Analyst</h5>
+                        <p>Dawid Gac</p>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 my-4 aboutRow">
-                <img src="./../assets/images/Kamil_Koncewicz.jpg" class="foto"/>
-                <div class="d-inline-block px-4">
-                    <h5>Graphic Designer</h5>
-                    <p>Kamil Koncewicz</p>
+            <div class="col-12 col-md-6 my-2 aboutRow px-0">
+                <div class="row m-0">
+                    <div class="col-12 col-md-6 heightImg ps-md-0">
+                        <img src="./../assets/images/Kamil_Koncewicz.jpg" class="foto h-100"/>
+                    </div>
+                    <div class="col-12 col-md-6 px-0 py-md-5">
+                        <h5>Graphic Designer</h5>
+                        <p>Kamil Koncewicz</p>
+                    </div>
                 </div>
             </div>
         </div>
